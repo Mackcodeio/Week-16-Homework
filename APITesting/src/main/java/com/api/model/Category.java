fb1b50aa8cd8978d8225a3da4ab9608e6d049b21
Mackcode <maheshhirpara.connect@gmail.com>
2023-07-12 21:28:13 +0100

    12/07/2023

--- APITesting/src/main/java/com/api/model/Category.java
+++ APITesting/src/main/java/com/api/model/Category.java
@@ -1,11 +1,17 @@
 package com.api.model;
 
-import java.util.ArrayList;
-
 public class Category {
+
+    public String id;
     private String name;
-    private ArrayList<SubCategory> subCategories;
-    private ArrayList<CategoryPath> categoryPath;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -14,22 +20,4 @@ public class Category {
     public void setName(String name) {
         this.name = name;
     }
-
-    public ArrayList<SubCategory> getSubCategories() {
-        return subCategories;
-    }
-
-    public void setSubCategories(ArrayList<SubCategory> subCategories) {
-        this.subCategories = subCategories;
-    }
-
-    public ArrayList<CategoryPath> getCategoryPath() {
-        return categoryPath;
-    }
-
-    public void setCategoryPath(ArrayList<CategoryPath> categoryPath) {
-        this.categoryPath = categoryPath;
-    }
-
-
 }
--- APITesting/src/test/java/com/api/categoryinfo/CategoryCRUDTest.java
+++ APITesting/src/test/java/com/api/categoryinfo/CategoryCRUDTest.java
@@ -1,19 +1,16 @@
 package com.api.categoryinfo;
 
 import com.api.model.Category;
-import com.api.model.CategoryPath;
-import com.api.model.SubCategory;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 import static io.restassured.RestAssured.given;
 
 public class CategoryCRUDTest {
-    static int idNumber;
-    //  static ValidatableResponse response;
+    static String idNumber;
+   // static ValidatableResponse response;
 
     @BeforeClass
     public static void init(){
@@ -31,25 +28,58 @@ public class CategoryCRUDTest {
     @Test //  post new and retrive ID
     public void test002(){
         Category category = new Category();
-        SubCategory subCategory = new SubCategory();
-        CategoryPath categoryPath = new CategoryPath();
 
-        ArrayList<String> sc = new ArrayList<String>();
-        sc.add("Apple");
-        sc.add("Mango");
-        sc.add("Orange");
-        sc.add("Banana");
-
-        ArrayList<String> cp = new ArrayList<String>();
-        cp.add("2 Apple");
-        cp.add("3 Mango");
-        cp.add("5 Orange");
-        cp.add("7 Banana");
-
+        category.setId("asd123562");
         category.setName("Fruits");
 
+         Response response= given()
+                .log().all()
+                .header("Content-Type","application/json")
+                 .body(category)
+                 .when()
+                 .post();
 
+        response.then().log().all().statusCode(201);
 
-
+        idNumber = response.then().extract().path("id");
+        System.out.println(idNumber);
     }
+
+    @Test // get single data
+    public void getSingleCategoryData(){
+        Response response = given()
+                .log().all()
+                .pathParam("id","asd123562")
+                .when()
+                .get("/{id}");
+        response.then().log().all().statusCode(200);
+    }
+
+    @Test  // Update
+    public void updateSingleCategoryData(){
+        Category category = new Category();
+        category.setId("AK47");
+        category.setName("Python Progamming");
+
+        Response response = given()
+                .log().all()
+                .header("Content-Type","application/json")
+                .pathParam("id","asd123562")
+                .body(category)
+                .when()
+                .patch("/{id}");
+                response.then().log().all().statusCode(200);
+    }
+
+    @Test// delete it
+    public void deleteCategoryData() {
+        Response response =given()
+                .log().all()
+                .header("Content-Type","application/json")
+                .pathParam("id","asd123562")
+                .when()
+                .delete("/{id}");
+        response.then().statusCode(200);
+    }
+
 }
